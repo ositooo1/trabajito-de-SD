634ccf4fb29a1134d9ea90443e818eb2e9424da3
ositooo1 <d48532356@alumnos.itr.edu.ar>
2024-09-12 14:14:41 -0300

    aDKLGKDBG

---
+++ locochon.js
@@ -0,0 +1,15 @@
+let img;
+
+function setup () {
+
+    createCanvas(1500, windowHeight);
+    background(img);
+    textSize(30)
+    text('<--------------- Simon', 900,300)
+    textSize(35)
+    text('HACELE CASO A SIMON',700,100)
+}
+function preload() {
+    img = loadImage('waaaa.jpeg')}
+    
+      
